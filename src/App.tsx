1c47346db03060bf0c971b6aef13b2b949eccb7d
Jorge Florian <jafn28@gmail.com>
2021-09-01 07:59:57 +0200

    create initial photo search feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,104 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from '@material-ui/core/Container'
+import TextField from '@material-ui/core/TextField';
+import Snackbar from '@material-ui/core/Snackbar';
+import ImageList from '@material-ui/core/ImageList'
+import ImageListItem from '@material-ui/core/ImageListItem';
+import Alert from '@material-ui/lab/Alert'
+import { createApi } from 'unsplash-js';
+import { Photos } from 'unsplash-js/dist/methods/search/types/response';
+import { useState } from 'react';
+import { Basic } from 'unsplash-js/dist/methods/photos/types';
+
+const browserApi = createApi({
+  // TODO: use proxy to secure access key
+  // apiUrl: 'https://mywebsite.com/unsplash-proxy',
+  accessKey: process.env.REACT_APP_UNSPLASH_ACCESS_KEY || '',
+});
+
+
+function useDebounceState<T>(state: T, delay: number) {
+  const [debouncedState, setDebouncedValue] = useState<T>(state);
+
+  React.useEffect(() => {
+      const handler = setTimeout(() => setDebouncedValue(state), delay);
+
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [state, delay] 
+  );
+
+  return debouncedState;
+}
+
+const usePhotoFetcher = (query: string) => {
+  const abortControllerRef = React.useRef(new AbortController())
+  const [photos, setPhotos] = useState<Basic[]>([])
+  const [errors, setErrors] = useState<string[]>([])
+  const [totalPages, setTotalPages] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
+  const debouncedQuery = useDebounceState(query, 500)
+  const PAGE_SIZE = 10
+
+  React.useEffect(() => {
+    abortControllerRef.current.abort()
+    abortControllerRef.current = new AbortController()
+    browserApi.search.getPhotos(
+      { query: debouncedQuery, page: currentPage, perPage: PAGE_SIZE },
+      { signal: abortControllerRef.current.signal }
+    ).then(
+      apiResponse => {
+        if (apiResponse.type === 'error') setErrors(apiResponse.errors)
+        else if (apiResponse.response) {
+          setPhotos(apiResponse.response.results)
+          setTotalPages(apiResponse.response.total_pages)
+        }
+      }
+    )
+  }, [debouncedQuery, currentPage])
+
+  return { photos, errors, totalPages }
+}
 
 function App() {
+  const [query, setQuery] = React.useState<string>('')
+  const { photos, errors } = usePhotoFetcher(query)
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newQuery = event.target.value
+    setQuery(newQuery)
+  }
+
+  const handleCloseErrors = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') return;
+    // setErrors([])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Snackbar open={Boolean(errors.length)} autoHideDuration={6000} onClose={handleCloseErrors}>
+        <Alert onClose={handleCloseErrors} severity="error">
+          <ul>
+            {errors.map(error => (<li key={error}>{error}</li>))}
+          </ul>
+        </Alert>
+      </Snackbar>
+      <form noValidate autoComplete="off">
+        <TextField label="Search for images" fullWidth value={query} onChange={handleQueryChange} />
+      </form>
+      { photos ?
+        <ImageList rowHeight={200} cols={5}>
+          {photos.map(p => (
+            <ImageListItem key={p.id} cols={1}>
+              <img src={p.urls.thumb} alt={p.alt_description || ''} />
+            </ImageListItem>
+          ))}
+        </ImageList>
+        : null
+      }
+    </Container>
   );
 }
 
