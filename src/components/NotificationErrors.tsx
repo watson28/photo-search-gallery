fb1b160bc5890c71013e7645a9f4e9511f7bde16
Jorge Florian <jorge.florian@sennder.com>
2021-09-05 19:32:08 +0200

    wrap component callbacks with useCallback

--- src/components/NotificationErrors.tsx
+++ src/components/NotificationErrors.tsx
@@ -1,22 +1,23 @@
 import Snackbar  from '@material-ui/core/Snackbar'
 import Alert from '@material-ui/lab/Alert'
+import { useCallback } from 'react'
 
 interface NotificationErrorsProps {
   errors: string[]
   onClose?(): void
 }
 
-const NotificationErrors = (props: NotificationErrorsProps) => {
-  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+const NotificationErrors = ({ errors, onClose }: NotificationErrorsProps) => {
+  const handleClose = useCallback((event?: React.SyntheticEvent, reason?: string) => {
     if (reason === 'clickaway') return
-    if (props.onClose) props.onClose()
-  }
+    if (onClose) onClose()
+  }, [onClose])
 
 	return (
-    <Snackbar open={Boolean(props.errors.length)} onClose={handleClose}>
+    <Snackbar open={Boolean(errors.length)} onClose={handleClose}>
       <Alert onClose={handleClose} severity="error">
         <ul>
-          {props.errors.map(error => (<li key={error}>{error}</li>))}
+          {errors.map(error => (<li key={error}>{error}</li>))}
         </ul>
       </Alert>
     </Snackbar>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,6 +3,7 @@ import InputAdornment from '@material-ui/core/InputAdornment'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import ImageSearchIcon from '@material-ui/icons/ImageSearch'
 import { makeStyles, createStyles, Theme } from '@material-ui/core'
+import { useCallback } from 'react'
 
 interface SearchBarProps {
   className?: string
@@ -24,16 +25,16 @@ const useStyle = makeStyles((theme: Theme) => {
   })
 })
 
-const SearchBar = (props: SearchBarProps) => {
+const SearchBar = ({ className, query, onChange, loading }: SearchBarProps) => {
   const classes = useStyle()
-  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleQueryChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault()
-    if (props.onChange) props.onChange(event.target.value)
-  }
+    if (onChange) onChange(event.target.value)
+  }, [onChange])
 
 	return (
     <form
-      className={props.className}
+      className={className}
       noValidate
       autoComplete="off"
       onSubmit={event => event.preventDefault()}
@@ -42,14 +43,14 @@ const SearchBar = (props: SearchBarProps) => {
       id="search-bar-text-field"
       label="Search for images"
       variant="outlined"
-      fullWidth value={props.query}
+      fullWidth value={query}
       onChange={handleQueryChange}
       InputProps={{
         startAdornment: (
         <InputAdornment position="start">
           <div className={classes.loadingWrapper}>
             <ImageSearchIcon />
-            { props.loading && <CircularProgress className={classes.progress} size={32} thickness={2} />}
+            { loading && <CircularProgress className={classes.progress} size={32} thickness={2} />}
           </div>
         </InputAdornment>
       )}}
